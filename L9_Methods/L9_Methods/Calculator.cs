68b7ebac13c1870c2d4e423c8b804788ec2c0102
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-11-21 17:15:28 -0500

    lesson 10 - methods

---
+++ L9_Methods/L9_Methods/Calculator.cs
@@ -0,0 +1,16 @@
+﻿namespace L9_Methods
+{
+    public class Calculator
+    {
+        public int Add(params int[] numbers)
+        {
+            var sum = 0;
+            foreach (var number in numbers)
+            {
+                sum += number;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ L9_Methods/L9_Methods/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L9_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var number = int.Parse("abc");
+
+            int number;
+            var result = int.TryParse("abc", out number);
+            if (result)
+                Console.WriteLine(number);
+            else
+                Console.WriteLine("Conversion failed.");
+        }
+
+        static void UseParams()
+        {
+            var calculator = new Calculator();
+            Console.WriteLine(calculator.Add(1, 2));
+            Console.WriteLine(calculator.Add(1, 2, 3, 4, 5, 6));
+            Console.WriteLine(calculator.Add(new int[] { 55, 66, 77, 88, 99 }));
+        }
+
+        static void UsePoints()
+        {
+            try
+            {
+                var point = new Point(10, 20);
+                point.Move(null);
+                Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+
+                point.Move(100, 200);
+                Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An unexpected error has occured.");
+            }
+        }
+    }
+}
