dc84b42f8dab63b5ed13a1a47d2287891e3f2bcb
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-11-21 16:06:32 -0500

    lesson 6 - classes

---
+++ L6_Classes/L6_Classes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L6_Classes
+{
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0}, I am {1}", to, Name);
+        }
+
+        public static Person Parse(string str)
+        {
+            var person = new Person();
+            person.Name = str;
+
+            return person;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = Person.Parse("John");
+            person.Introduce("Steve");
+        }
+    }
+}
