0e52b6037f67604209ab6e43881ae59362c263e4
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-11-22 17:14:54 -0500

    lesson 21 - access modifiers #2

---
+++ L21_Access_Modifiers_2/Amazon/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Amazon
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public void Promote()
+        {
+            var rating = CalculateRating(excludeOrders: true);
+            Console.WriteLine(rating == 0 ? "Promoted to Level 1" : "Promoted to Level 2");
+        }
+
+        private int CalculateRating(bool excludeOrders)
+        {
+            return 0;
+        }
+    }
+}
---
+++ L21_Access_Modifiers_2/Amazon/RateCalculator.cs
@@ -0,0 +1,10 @@
+﻿namespace Amazon
+{
+    internal class RateCalculator
+    {
+        public int Calculate()
+        {
+            return 0;
+        }
+    }
+}
