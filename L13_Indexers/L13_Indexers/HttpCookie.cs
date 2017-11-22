65ce0d62357796bb1f7f5781167d098c62928162
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-11-22 10:23:06 -0500

    lesson 13 - indexers

---
+++ L13_Indexers/L13_Indexers/HttpCookie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace L13_Indexers
+{
+    public class HttpCookie
+    {
+        private readonly Dictionary<string, string> _dictionary;
+
+        public HttpCookie()
+        {
+            _dictionary = new Dictionary<string, string>();
+        }
+
+        public string this[string key]
+        {
+            get { return _dictionary[key]; }
+            set { _dictionary[key] = value; }
+        }
+    }
+}
---
+++ L13_Indexers/L13_Indexers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L13_Indexers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cookie = new HttpCookie();
+            cookie["name"] = "Steve";
+            Console.WriteLine(cookie["name"]);
+        }
+    }
+}
