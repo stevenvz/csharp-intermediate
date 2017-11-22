42d1f6610a6d8a58b180b40eb759cfd320e8e81d
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-11-21 20:51:36 -0500

    lesson 11 - access modifiers

---
+++ L_11_Access_Modifiers/L_11_Access_Modifiers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L_11_Access_Modifiers
+{
+    public class Person
+    {
+        private DateTime _birthdate;
+
+        public void SetBirthdate(DateTime birthdate)
+        {
+            _birthdate = birthdate;
+        }
+
+        public DateTime GetBirthdate()
+        {
+            return _birthdate;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.SetBirthdate(new DateTime(1980, 12, 6));
+            Console.WriteLine(person.GetBirthdate());
+        }
+    }
+}
