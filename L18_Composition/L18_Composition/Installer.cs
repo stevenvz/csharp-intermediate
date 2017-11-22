3000de4a939d982e071eb837487f728c7580cc65
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-11-22 13:44:39 -0500

    lesson 18 - composition

---
+++ L18_Composition/L18_Composition/Installer.cs
@@ -0,0 +1,17 @@
+﻿namespace L18_Composition
+{
+    public class Installer
+    {
+        private readonly Logger _logger;
+
+        public Installer(Logger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Install()
+        {
+            _logger.Log("We are installing the application.");
+        }
+    }
+}
---
+++ L18_Composition/L18_Composition/Logger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace L18_Composition
+{
+    public class Logger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
