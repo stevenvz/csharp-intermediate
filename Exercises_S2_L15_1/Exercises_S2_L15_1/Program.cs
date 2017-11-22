6277a204b51f5577464e968d7c4b778f4af2efac
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-11-22 11:41:42 -0500

    exercise - section 2 lesson 15 #1

---
+++ Exercises_S2_L15_1/Exercises_S2_L15_1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises_S2_L15_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+
+            //error handling behavior directed by exercise specifications - see Stopwatch.cs for details
+
+            while (true)
+            {
+                Console.Write("Enter \'s\' to START, \'x\' to STOP, or \'q\' to QUIT: ");
+                var input = Console.ReadLine();
+
+                if (input == "q")
+                {
+                    Console.WriteLine("Bye!");
+                    break;
+                }
+                else if (input == "s")
+                {
+                    Console.WriteLine("====================================================");
+                    Console.WriteLine(stopwatch.Start());
+                }
+                else if (input == "x")
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Result (HH:MM:SS.ms): " + stopwatch.Stop());
+                    Console.WriteLine();
+                    Console.WriteLine("====================================================");
+                }
+            }
+        }
+    }
+}
---
+++ Exercises_S2_L15_1/Exercises_S2_L15_1/Stopwatch.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Exercises_S2_L15_1
+{
+    public class Stopwatch
+    {
+        private DateTime _start;
+        private DateTime _stop;
+
+        //to prevent stopwatch from being started twice
+        private bool _isRunning;
+        
+        //start method
+        public string Start()
+        {
+            if (!_isRunning)
+            {
+                _start = DateTime.Now;
+                _isRunning = true;
+                return "Stopwatch started @ " + _start;
+            }
+            else
+            {
+                //exercise specifications directed the below behavior, otherwise I would have 
+                //simply informed the user & continued the main loop
+                throw new InvalidOperationException("Stopwatch has already been started.");
+            }
+        }
+
+        //stop method
+        public string Stop()
+        {
+            if (_isRunning)
+            {
+                _stop = DateTime.Now;
+                _isRunning = false;
+                return (_stop - _start).ToString();
+            }
+            else
+            {
+                //exercise specifications directed the below behavior, otherwise I would have 
+                //simply informed the user & continued the main loop
+                throw new InvalidOperationException("Stopwatch has not been started.");
+            }
+        }
+
+    }
+}
