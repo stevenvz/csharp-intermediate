99d7f7d2ea7185becd350001eeccef3eb873cab9
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-11-22 17:52:35 -0500

    lesson 22 - constructors & inheritance

---
+++ L22_Constructors_and_Inheritance/L22_Constructors_and_Inheritance/Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace L22_Constructors_and_Inheritance
+{
+    public class Car : Vehicle
+    {
+        public Car(string registrationNumber)
+            : base(registrationNumber)
+        {
+            Console.WriteLine("Car is being initialized. {0}", registrationNumber);
+        }
+    }
+}
---
+++ L22_Constructors_and_Inheritance/L22_Constructors_and_Inheritance/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L22_Constructors_and_Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car("Z857");
+        }
+    }
+}
