f373e62313c4375c8ca7c7e9ccdc94c72587cfcc
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-11-22 12:22:50 -0500

    exercise - section 2 lesson 15 #2

---
+++ Exercises_S2_L15_2/Exercises_S2_L15_2/Post.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exercises_S2_L15_2
+{
+    public class Post
+    {
+        public Post(string title, string description)
+        {
+            Title = title;
+            Description = description;
+            Created = DateTime.Now;
+        }
+
+        private int _votes;
+
+        public string Title { get; }
+        public string Description { get; set; }
+        public DateTime Created { get; }
+
+        public int Votes => _votes;
+
+        public int UpVote()
+        {
+            _votes += 1;
+            return _votes;
+        }
+
+        public int DownVote()
+        {
+            _votes -= 1;
+            return _votes;
+        }
+    }
+}
---
+++ Exercises_S2_L15_2/Exercises_S2_L15_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises_S2_L15_2
+{
+    class Program
+    {
+        //exercise specification is to simply create a Post class & use it to up-vote/down-vote/get votes
+
+        static void Main(string[] args)
+        {
+            var post = new Post("How to Foo the Bar", "Lorem Ipsum blah blah blah");
+            post.UpVote();
+            post.UpVote();
+            post.UpVote();
+            post.UpVote();
+            post.DownVote();
+            post.DownVote();
+
+            Console.WriteLine("Vote value: " + post.Votes);
+        }
+    }
+}
