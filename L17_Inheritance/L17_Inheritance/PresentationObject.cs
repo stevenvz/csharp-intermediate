df87f8b94c6b5d05c9a4be882daa4a10d244ccf3
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-11-22 13:32:33 -0500

    lesson 17 - inheritance

---
+++ L17_Inheritance/L17_Inheritance/PresentationObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace L17_Inheritance
+{
+    public class PresentationObject
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public void Copy()
+        {
+            Console.WriteLine("Object copied to clipboard.");
+        }
+
+        public void Duplicate()
+        {
+            Console.WriteLine("Object was duplicated.");
+        }
+    }
+}
