9fda6e759eab6bdac3cb9ef8568d07396f0a443d
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-11-21 23:03:30 -0500

    lesson 12 - properties

---
+++ L12_Properties/L12_Properties/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L12_Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person(new DateTime(1865, 12, 6));
+            Console.WriteLine(person.Age);
+        }
+    }
+}
