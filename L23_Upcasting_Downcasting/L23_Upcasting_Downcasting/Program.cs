0aacfbe55da50782ae4e9b15155b19f279340ce9
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-11-23 09:53:14 -0500

    lesson 23 - upcasting & downcasting

---
+++ L23_Upcasting_Downcasting/L23_Upcasting_Downcasting/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L23_Upcasting_Downcasting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape shape = new Text();
+            Text text = (Text) shape;
+        }
+    }
+}
---
+++ L23_Upcasting_Downcasting/L23_Upcasting_Downcasting/Text.cs
@@ -0,0 +1,8 @@
+﻿namespace L23_Upcasting_Downcasting
+{
+    public class Text : Shape
+    {
+        public int FontSize { get; set; }
+        public string FontName { get; set; }
+    }
+}
