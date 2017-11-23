50f51450b45c3f586195876fc65f23d5165b5705
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-11-23 10:05:00 -0500

    lesson 24 - boxing & unboxing

---
+++ L24_Boxing_Unboxing/L24_Boxing_Unboxing/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L24_Boxing_Unboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new ArrayList();
+            list.Add(1);
+            list.Add("Steve");
+            list.Add(DateTime.Today);
+
+            var anotherList = new List<int>();
+            var names = new List<string>();
+            names.Add("Joe");
+        }
+    }
+}
