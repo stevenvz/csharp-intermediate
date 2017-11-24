406cc543cf6f3915f8eb16c1906551a9e423d830
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-11-23 19:48:30 -0500

    exercise - section 4 lesson 26

---
+++ Exercises_S4_L26/Exercises_S4_L26/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises_S4_L26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            stack.Clear();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push("Steve");
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
---
+++ Exercises_S4_L26/Exercises_S4_L26/Stack.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+
+namespace Exercises_S4_L26
+{
+    public class Stack
+    {
+        private ArrayList _stackList = new ArrayList();
+        private int _index;
+
+        public void Push(object obj)
+        {
+            if (obj != null)
+            {
+                _stackList.Add(obj);
+                _index++;
+            }
+            else
+            {
+                throw new InvalidOperationException("Cannot pass NULL to the Stack");
+            }
+            
+        }
+
+        public object Pop()
+        {
+            if (_stackList.Count != 0)
+            {
+                _index--;
+                var returnObject = _stackList[_index];
+                _stackList.RemoveAt(_index);
+                return returnObject;
+            }
+
+            throw new InvalidOperationException("Cannot remove from an empty Stack");
+        }
+
+        public void Clear()
+        {
+            if (_stackList.Count != 0)
+            {
+                _stackList.Clear();
+                _index = 0;
+            }
+            else
+            {
+                throw new InvalidOperationException("Cannot remove from an empty Stack");
+            }
+        }
+    }
+}
