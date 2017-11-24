4563c4269bde9993f6e806c4f6679ae9e9ed8a72
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-11-24 16:59:51 -0500

    exercise - section 5 lesson 31 #1

---
+++ Exercises_S5_L31/Exercises_S5_L31/DbConnection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace Exercises_S5_L31
+{
+    public abstract class DbConnection
+    {
+        public string _connectionString;
+        public TimeSpan _timeout;
+
+        protected DbConnection(string connectionString)
+        {
+            if (String.IsNullOrEmpty(connectionString))
+            {
+                throw new InvalidDataException("Connection string cannot be null or empty.");
+            }
+            _connectionString = connectionString;
+            _timeout = TimeSpan.FromMilliseconds(2000);
+        }
+        
+        public abstract void OpenConnnection();
+        public abstract void CloseConnection();
+    }
+}
---
+++ Exercises_S5_L31/Exercises_S5_L31/SqlConnection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercises_S5_L31
+{
+    public class SqlConnection : DbConnection
+    {
+        public SqlConnection(string connectionString) : base(connectionString)
+        {
+        }
+
+        public override void OpenConnnection()
+        {
+            Console.WriteLine("SqlConnection open: " + _connectionString);
+        }
+
+        public override void CloseConnection()
+        {
+            Console.WriteLine("SqlConnection closed.");
+        }
+    }
+}
