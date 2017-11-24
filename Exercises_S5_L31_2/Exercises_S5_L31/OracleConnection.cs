45b5cb8f32c78e37f63ba9c04d2e1d38eb9823a5
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-11-24 17:07:04 -0500

    exercise - section 5 lesson 31 #1

---
+++ Exercises_S5_L31_2/Exercises_S5_L31/OracleConnection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercises_S5_L31
+{
+    public class OracleConnection : DbConnection
+    {
+        public OracleConnection(string connectionString) : base(connectionString)
+        {
+        }
+
+        public override void OpenConnnection()
+        {
+            Console.WriteLine("OracleConnection open: " + _connectionString);
+        }
+
+        public override void CloseConnection()
+        {
+            Console.WriteLine("OracleConnection closed.");
+        }
+    }
+}
---
+++ Exercises_S5_L31_2/Exercises_S5_L31/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises_S5_L31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var oracleConnection = new OracleConnection("Oracle connection string");
+            oracleConnection.OpenConnnection();
+            oracleConnection.CloseConnection();
+
+            var sqlConnection = new SqlConnection("SQL connection string");
+            sqlConnection.OpenConnnection();
+            sqlConnection.CloseConnection();
+        }
+    }
+}
